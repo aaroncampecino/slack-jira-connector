3dca9bb2d276b01b00c78b00d388fd908c453a03
Aaron Campecino <aaroncampecino@gmail.com>
2022-11-06 17:26:08 +0800

    added installationStore

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const { App, LogLevel } = require("@slack/bolt");
 import "dotenv/config";
 import { registerListeners } from "./listeners";
 import { logger } from "./logger";
+const orgAuth = require("./database/auth/store_user_org_install");
+const workspaceAuth = require("./database/auth/store_user_workspace_install");
+const db = require("./database/db");
+const dbQuery = require("./database/find_user");
+const customRoutes = require("./utility/custom_routes");
 
 let logLevel;
 switch (process.env.LOG_LEVEL) {
@@ -24,19 +29,47 @@ switch (process.env.LOG_LEVEL) {
 const app = new App({
   signingSecret: process.env.SLACK_BOT_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
+  clientId: process.env.SLACK_CLIENT_ID,
   // appToken: process.env.SLACK_APP_TOKEN,
   // socketMode: process.env.SLACK_SOCKET_MODE === "true",
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
   logLevel: logLevel,
-  customRoutes: [
-    {
-      path: "/health-check",
-      method: ["GET"],
-      handler: (req, res) => {
-        res.writeHead(200);
-        res.end("UP!");
-      },
+  customRoutes: customRoutes,
+  stateSecret: "aaron-the-great",
+  installerOptions: {
+    stateVerification: false,
+    directInstall: true,
+  },
+  installationStore: {
+    storeInstallation: async (installation) => {
+      console.log("installation: " + installation);
+      console.log(installation);
+      if (
+        installation.isEnterpriseInstall &&
+        installation.enterprise !== undefined
+      ) {
+        return orgAuth.saveUserOrgInstall(installation);
+      }
+      if (installation.team !== undefined) {
+        return workspaceAuth.saveUserWorkspaceInstall(installation);
+      }
+      throw new Error("Failed saving installation data to installationStore");
     },
-  ],
+    fetchInstallation: async (installQuery) => {
+      console.log("installQuery: " + installQuery);
+      console.log(installQuery);
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        return dbQuery.findUser(installQuery.enterpriseId);
+      }
+      if (installQuery.teamId !== undefined) {
+        return dbQuery.findUser(installQuery.teamId);
+      }
+      throw new Error("Failed fetching installation");
+    },
+  },
 });
 
 registerListeners(app);
@@ -45,4 +78,5 @@ registerListeners(app);
   // Start the app
   await app.start(process.env.PORT);
   logger.info(`⚡️ Bolt apps is running on port ${process.env.PORT}!`);
+  db.connect();
 })();
