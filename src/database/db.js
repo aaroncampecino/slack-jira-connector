91b176b688bd4380e7bf2398fb7e293c15d35dfc
Aaron Campecino <aaroncampecino@gmail.com>
2022-11-26 10:59:59 +0800

    comment out schema on db.js

--- src/database/db.js
+++ src/database/db.js
@@ -17,48 +17,48 @@ const connect = async function () {
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 };
 
-const usersSchema = mongoose.Schema(
-  {
-    _id: String,
-    team: { id: String, name: String },
-    enterprise: { id: String, name: String },
-    user: {
-      token: String,
-      scopes: [String],
-      id: String,
-      refreshToken: String,
-      expiresAt: Number,
-    },
-    tokenType: String,
-    isEnterpriseInstall: Boolean,
-    appId: String,
-    authVersion: String,
-    bot: {
-      scopes: [String],
-      token: String,
-      userId: String,
-      id: String,
-      refreshToken: String,
-      expiresAt: Number,
-    },
-    incomingWebhook: {
-      url: String,
-      channel: String,
-      channelId: String,
-      configurationUrl: String,
-    },
-  },
-  { _id: false }
-);
+// const usersSchema = mongoose.Schema(
+//   {
+//     _id: String,
+//     team: { id: String, name: String },
+//     enterprise: { id: String, name: String },
+//     user: {
+//       token: String,
+//       scopes: [String],
+//       id: String,
+//       refreshToken: String,
+//       expiresAt: Number,
+//     },
+//     tokenType: String,
+//     isEnterpriseInstall: Boolean,
+//     appId: String,
+//     authVersion: String,
+//     bot: {
+//       scopes: [String],
+//       token: String,
+//       userId: String,
+//       id: String,
+//       refreshToken: String,
+//       expiresAt: Number,
+//     },
+//     incomingWebhook: {
+//       url: String,
+//       channel: String,
+//       channelId: String,
+//       configurationUrl: String,
+//     },
+//   },
+//   { _id: false }
+// );
 
-const channelsSchema = mongoose.Schema({
-  _id: String, //team id of client workspace
-  fromChannelId: String, //channel id of admin workspace
-  toChannelId: String, //channel id of client workspace
-});
+// const channelsSchema = mongoose.Schema({
+//   _id: String, //team id of client workspace
+//   fromChannelId: String, //channel id of admin workspace
+//   toChannelId: String, //channel id of client workspace
+// });
 
-const User = mongoose.model("User", usersSchema);
-const Channel = mongoose.model("Channel", channelsSchema);
+// const User = mongoose.model("User", usersSchema);
+// const Channel = mongoose.model("Channel", channelsSchema);
 
 module.exports = {
   connect,
